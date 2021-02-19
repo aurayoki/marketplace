bb4bb3a2a58ac639bdd15b06e44e82777882adfb
Semyon_Fomichyov <fomichyov.semyon@gmail.com>
2021-02-19 18:51:48 +0300

    Fixes.

--- src/test/java/com/jm/marketplace/service/user/UserServiceTest.java
+++ src/test/java/com/jm/marketplace/service/user/UserServiceTest.java
@@ -35,7 +35,7 @@ public class UserServiceTest {
 
     UserDto userDto = new UserDto(1L, "Tommy", "Garposs", "1234", "asd@gmail.com",
             new CityDto(1L, "Piter"), LocalDate.of(1992, 6, 2),
-            "89679681234", "null", null, null, false, null);
+            "89679681234", "null", null, null, null, false, null);
 
     User userTest = new User(1L, "Tommy", "Garposs", "1234", "asd@gmail.com",
             new City(1L, "Piter"), LocalDate.of(1992, 6, 2),

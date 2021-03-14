d403ba360b19fb1acea85b9c9c412cbbd47aec4b
Aurayoki <aurayoki.s@gmail.com>
2021-03-14 16:41:31 +0300

    Тест для GeoCoderService и правки

--- src/test/java/com/jm/marketplace/util/geo/GeoRoutesServiceTest.java
+++ src/test/java/com/jm/marketplace/util/geo/GeoRoutesServiceTest.java
@@ -1,24 +1,15 @@
 package com.jm.marketplace.util.geo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class GeoRoutesServiceTest {
-
-
-    String adr1 = "Санкт-Петербург, Маршала казакова 55";
-    String adr2 = "Санкт-Петербург, Ленинский-проспект 81";
-
-    @Autowired
-    GeoRoutesService geoRoutesService;
 
     @Test
     void getDistanceByCity() {
          /*
        Написать тесты
         */
-        System.out.println(geoRoutesService.getDistanceByCity(adr1, adr2));
     }
 }

ae65431c22e53225cfb474fbc6365197a80290bf
Aurayoki <aurayoki.s@gmail.com>
2021-03-26 21:00:44 +0300

    Поправил секьюрити

--- src/main/java/com/jm/marketplace/config/swagger/SpringFoxConfig.java
+++ src/main/java/com/jm/marketplace/config/swagger/SpringFoxConfig.java
@@ -2,8 +2,11 @@ package com.jm.marketplace.config.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +21,17 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.jm.marketplace.controller.rest"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+        return new ApiInfoBuilder()
+                .title("Employee Profile Api")
+                .description("This API can be used to get profile level actions and information for an Employee")
+                .version("V1.0")
+                .contact(new Contact("Do you want to understand how it works? Click on me!",
+                        "https://javalearn.online", "sevostyanovg.d@gmail.com"))
                 .build();
     }
 }

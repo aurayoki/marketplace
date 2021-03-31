132556ba61910b0ffa0ccc52f8213a258bc19308
Irkkuev Sagid <Irkuev666@gmail.com>
2021-03-31 21:46:13 +0300

    Добавил управление транзакциями в channelService

--- src/main/java/com/jm/marketplace/service/chat/chatchannel/ChatChannelServiceImpl.java
+++ src/main/java/com/jm/marketplace/service/chat/chatchannel/ChatChannelServiceImpl.java
@@ -4,10 +4,12 @@ import com.jm.marketplace.dao.ChatChannelDao;
 import com.jm.marketplace.model.chat.channel.ChatChannel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 public class ChatChannelServiceImpl implements ChatChannelService {
     private ChatChannelDao chatChannelDao;
 
